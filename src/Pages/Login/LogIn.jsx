757328d73877da16110ea64eb59566d5ac044a89
walexy026 <jao.olaniyi@gmail.com>
2023-08-07 15:10:22 +0100

    login

--- src/Pages/Login/LogIn.jsx
+++ src/Pages/Login/LogIn.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import "./LogIn.css";
 import pics from "../../Assets/itechLogInPics.svg";
+import { FcGoogle } from "react-icons/fc";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 const LogIn = () => {
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(AiOutlineEyeInvisible);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleToggle = () => {
+    if (type === "password") {
+      setIcon(AiOutlineEye);
+      setType("text");
+    } else {
+      setIcon(AiOutlineEyeInvisible);
+      setType("password");
+    }
+  };
   return (
     <div className="logInFlex">
-      <div>
+      <div className="firstFlex">
         <h2>Organize your workflow, boost your productivity</h2>
         <img src={pics} alt="logIn" />
       </div>
@@ -13,23 +32,50 @@ const LogIn = () => {
           <h3>Brand logo</h3>
           <span>Sign In</span>
           <span>Sign Up</span>
-          <button className="authBtn">Sign in with Google</button>
-          <form className="formDiv" action="">
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="email"
-              name=""
-              id="email"
-              placeholder="Insert your email"
-            />
+          <button className="authBtn">
+            <FcGoogle size="1.5rem" /> Sign in with Google
+          </button>
+          <form className="formDiv" action="" onSubmit={handleSubmit}>
+            <label htmlFor="email">
+              Email Address
+              <div className="inputWrapper">
+                <input
+                  type="email"
+                  name=""
+                  id="email"
+                  placeholder="Insert your email"
+                />
+              </div>
+            </label>
+            <label htmlFor="password">
+              Password
+              <div className="inputWrapper">
+                <input
+                  type={type}
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete="current-password"
+                  id="password"
+                  placeholder="Insert your password"
+                />
 
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name=""
-              id="password"
-              placeholder="Insert your password"
-            />
+                {
+                  <AiOutlineEyeInvisible
+                    handleSubmit
+                    // icon={icon}
+                    onClick={handleToggle}
+                    style={{
+                      fontSize: "2rem",
+                      position: "absolute",
+                      right: "20px",
+                      cursor: "pointer",
+                    }}
+                  />
+                }
+              </div>
+            </label>
+            {/* <AiOutlineEyeInvisible /> */}
             <div className="checkBoxFlex">
               <label htmlFor="loggedIn">
                 <input type="checkbox" name="" id="loggedIn" />
@@ -37,9 +83,7 @@ const LogIn = () => {
               </label>
               <a href="">Forget Password</a>
             </div>
-            <button className="loginBtn" type="submit">
-              Sign In
-            </button>
+            <button className="loginBtn">Sign In</button>
           </form>
         </div>
       </div>
